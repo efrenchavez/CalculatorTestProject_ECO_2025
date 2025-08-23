983c3ab8e3f99bdfccebda0ca079806922295a1d
efrenchavez <efrenchavezochoa@gmail.com>
2025-08-22 22:52:06 -0600

    Second manual commit. Testing before wrapping up for the day.

--- BasicCalculatorProject/src/efren/learning/calculator/CalculatorLauncher.java
+++ BasicCalculatorProject/src/efren/learning/calculator/CalculatorLauncher.java
@@ -3,8 +3,8 @@ package efren.learning.calculator;
 public class CalculatorLauncher {
 	
 	public static void main(String args[]) {
-		// version 0.0.0 does nothing
-		
+		// version 0.0.1 outputs message to console
+		System.out.println("Hello new project");
 	}
 
 }
