d8137311854aff2d4c3ffa8f13835c4fadf37080
efrenchavez <efrenchavezochoa@gmail.com>
2025-08-26 16:15:37 -0600

    Changed main class to a JFrame to set it up as the GUI.

--- BasicCalculatorProject/src/efren/learning/calculator/CalculatorLauncher.java
+++ BasicCalculatorProject/src/efren/learning/calculator/CalculatorLauncher.java
@@ -1,10 +1,42 @@
 package efren.learning.calculator;
 
-public class CalculatorLauncher {
-	
-	public static void main(String args[]) {
-		// version 0.0.1 outputs message to console
-		System.out.println("Hello new project");
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class CalculatorLauncher extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Version 0.0.2 changed main class to a JFRame
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CalculatorLauncher frame = new CalculatorLauncher();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CalculatorLauncher() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+
 	}
 
 }
